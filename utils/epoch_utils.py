609aef370b96df5b4f4a4a65620bc1523f86858c
cgm <501704164@qq.com>
2024-04-21 23:02:28 +0800

    update

--- utils/epoch_utils.py
+++ utils/epoch_utils.py
@@ -1,6 +1,159 @@
 import torch
 from tqdm import tqdm
 from utils.history_collect import AverageMeter
+from ops.metric.DetectionMetric import DetectionMetric
+from ops.detection.postprocess_utils import YoloPostProcess
+from torchvision.ops.boxes import box_convert, box_iou
+import numpy as np
+
+
+def clip_boxes(boxes, shape):
+    # Clip boxes (xyxy) to image shape (height, width)
+    if isinstance(boxes, torch.Tensor):  # faster individually
+        boxes[..., 0].clamp_(0, shape[1])  # x1
+        boxes[..., 1].clamp_(0, shape[0])  # y1
+        boxes[..., 2].clamp_(0, shape[1])  # x2
+        boxes[..., 3].clamp_(0, shape[0])  # y2
+    else:  # np.array (faster grouped)
+        boxes[..., [0, 2]] = boxes[..., [0, 2]].clip(0, shape[1])  # x1, x2
+        boxes[..., [1, 3]] = boxes[..., [1, 3]].clip(0, shape[0])  # y1, y2
+
+
+def scale_boxes(img1_shape, boxes, img0_shape, ratio_pad=None):
+    # Rescale boxes (xyxy) from img1_shape to img0_shape
+    if ratio_pad is None:  # calculate from img0_shape
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+
+    boxes[..., [0, 2]] -= pad[0]  # x padding
+    boxes[..., [1, 3]] -= pad[1]  # y padding
+    boxes[..., :4] /= gain
+    clip_boxes(boxes, img0_shape)
+    return boxes
+
+
+def process_batch(detections, labels, iouv):
+    """
+    Return correct prediction matrix
+    Arguments:
+        detections (array[N, 6]), x1, y1, x2, y2, conf, class
+        labels (array[M, 5]), class, x1, y1, x2, y2
+    Returns:
+        correct (array[N, 10]), for 10 IoU levels
+    """
+    correct = np.zeros((detections.shape[0], iouv.shape[0])).astype(bool)
+    iou = box_iou(labels[:, 1:], detections[:, :4])
+    correct_class = labels[:, 0:1] == detections[:, 5]
+    for i in range(len(iouv)):
+        x = torch.where((iou >= iouv[i]) & correct_class)  # IoU > threshold and classes match
+        if x[0].shape[0]:
+            matches = torch.cat((torch.stack(x, 1), iou[x[0], x[1]][:, None]), 1).cpu().numpy()  # [label, detect, iou]
+            if x[0].shape[0] > 1:
+                matches = matches[matches[:, 2].argsort()[::-1]]
+                matches = matches[np.unique(matches[:, 1], return_index=True)[1]]
+                # matches = matches[matches[:, 2].argsort()[::-1]]
+                matches = matches[np.unique(matches[:, 0], return_index=True)[1]]
+            correct[matches[:, 1].astype(int), i] = True
+    return torch.tensor(correct, dtype=torch.bool, device=iouv.device)
+
+
+def smooth(y, f=0.05):
+    # Box filter of fraction f
+    nf = round(len(y) * f * 2) // 2 + 1  # number of filter elements (must be odd)
+    p = np.ones(nf // 2)  # ones padding
+    yp = np.concatenate((p * y[0], y, p * y[-1]), 0)  # y padded
+    return np.convolve(yp, np.ones(nf) / nf, mode="valid")  # y-smoothed
+
+
+def compute_ap(recall, precision):
+    """Compute the average precision, given the recall and precision curves
+    # Arguments
+        recall:    The recall curve (list)
+        precision: The precision curve (list)
+    # Returns
+        Average precision, precision curve, recall curve
+    """
+
+    # Append sentinel values to beginning and end
+    mrec = np.concatenate(([0.0], recall, [1.0]))
+    mpre = np.concatenate(([1.0], precision, [0.0]))
+
+    # Compute the precision envelope
+    mpre = np.flip(np.maximum.accumulate(np.flip(mpre)))
+
+    # Integrate area under curve
+    method = "interp"  # methods: 'continuous', 'interp'
+    if method == "interp":
+        x = np.linspace(0, 1, 101)  # 101-point interp (COCO)
+        ap = np.trapz(np.interp(x, mrec, mpre), x)  # integrate
+    else:  # 'continuous'
+        i = np.where(mrec[1:] != mrec[:-1])[0]  # points where x axis (recall) changes
+        ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])  # area under curve
+
+    return ap, mpre, mrec
+
+
+def ap_per_class(tp, conf, pred_cls, target_cls, eps=1e-16):
+    """
+    Compute the average precision, given the recall and precision curves.
+
+    Source: https://github.com/rafaelpadilla/Object-Detection-Metrics.
+    # Arguments
+        tp:  True positives (nparray, nx1 or nx10).
+        conf:  Objectness value from 0-1 (nparray).
+        pred_cls:  Predicted object classes (nparray).
+        target_cls:  True object classes (nparray).
+        plot:  Plot precision-recall curve at mAP@0.5
+        save_dir:  Plot save directory
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+
+    # Sort by objectness
+    i = np.argsort(-conf)
+    tp, conf, pred_cls = tp[i], conf[i], pred_cls[i]
+
+    # Find unique classes
+    unique_classes, nt = np.unique(target_cls, return_counts=True)
+    nc = unique_classes.shape[0]  # number of classes, number of detections
+
+    # Create Precision-Recall curve and compute AP for each class
+    px, py = np.linspace(0, 1, 1000), []  # for plotting
+    ap, p, r = np.zeros((nc, tp.shape[1])), np.zeros((nc, 1000)), np.zeros((nc, 1000))
+    for ci, c in enumerate(unique_classes):
+        i = pred_cls == c
+        n_l = nt[ci]  # number of labels
+        n_p = i.sum()  # number of predictions
+        if n_p == 0 or n_l == 0:
+            continue
+
+        # Accumulate FPs and TPs
+        fpc = (1 - tp[i]).cumsum(0)
+        tpc = tp[i].cumsum(0)
+
+        # Recall
+        recall = tpc / (n_l + eps)  # recall curve
+        r[ci] = np.interp(-px, -conf[i], recall[:, 0], left=0)  # negative x, xp because xp decreases
+
+        # Precision
+        precision = tpc / (tpc + fpc)  # precision curve
+        p[ci] = np.interp(-px, -conf[i], precision[:, 0], left=1)  # p at pr_score
+
+        # AP from recall-precision curve
+        for j in range(tp.shape[1]):
+            ap[ci, j], mpre, mrec = compute_ap(recall[:, j], precision[:, j])
+
+    # Compute F1 (harmonic mean of precision and recall)
+    f1 = 2 * p * r / (p + r + eps)
+
+    i = smooth(f1.mean(0), 0.1).argmax()  # max F1 index
+    p, r, f1 = p[:, i], r[:, i], f1[:, i]
+    tp = (r * nt).round()  # true positives
+    fp = (tp / (p + eps) - tp).round()  # false positives
+    return tp, fp, p, r, f1, ap, unique_classes.astype(int)
 
 
 def train_epoch(model, loader, device, epoch, optimizer, criterion, accumulate=1):
@@ -53,18 +206,79 @@ def val_epoch(model, loader, device, epoch, criterion):
     stream = tqdm(loader)
 
     metric = {
-        'loss': AverageMeter(),
+        'lbox': AverageMeter(),
+        'lobj': AverageMeter(),
+        'lcls': AverageMeter(),
+        'mp': AverageMeter(),
+        'mr': AverageMeter(),
+        'map': AverageMeter(),
+        'map50': AverageMeter(),
         'epoch': epoch,
     }
 
-    for i, data in enumerate(stream, start=1):
+    jdict, stats, ap, ap_class = [], [], [], []
+
+    seen = 0
+    iouv = torch.linspace(0.5, 0.95, 10, device=device)  # iou vector for mAP@0.5:0.95
+    niou = iouv.numel()
+    single_cls = False
+    detect_metric = DetectionMetric(device)
+    post_process = YoloPostProcess(device, conf_thres=0.001, iou_thres=0.5)
+    tp, fp, p, r, f1, mp, mr, map50, ap50, map = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+
+    for i, data in enumerate(stream):
         images, targets = data
 
-        preds = model(images.to(device))
+        image_size = torch.as_tensor(images.shape[2:])
 
-        loss = criterion(preds, targets.to(device))
+        train_out = model(images.to(device))
 
-        metric['loss'].update(loss.detach().item())
-        stream.set_postfix(**metric)
+        _, lbox, lobj, lcls = criterion(train_out, targets.to(device), image_size.to(device))
+
+        metric['lbox'].update(lbox.item())
+        metric['lobj'].update(lobj.item())
+        metric['lcls'].update(lcls.item())
+
+        preds = post_process(train_out, criterion.anchors, image_size)
+        detect_metric(preds, targets.to(device), image_size[0], image_size[1])
+
+        for si, pred in enumerate(preds):
+            for target in targets:
+                target = target[target[:, 1] > 0].to(device)
+                labels = target[target[:, 0] == si, 1:]
+                labels[:, 0] = labels[:, 0] - 1
+                nl, npr = labels.shape[0], pred.shape[0]  # number of labels, predictions
+                correct = torch.zeros(npr, niou, dtype=torch.bool, device=device)  # init
+                seen += 1
+
+                if npr == 0:
+                    if nl:
+                        stats.append((correct, *torch.zeros((2, 0), device=device), labels[:, 0]))
+                    continue
+
+                if single_cls:
+                    pred[:, 5] = 0
+
+                predn = pred.clone()
+
+                if nl:
+                    tbox = box_convert(labels[:, 1:5], 'cxcywh', 'xyxy')  # target boxes
+                    labelsn = torch.cat((labels[:, 0:1], tbox), 1)
+                    correct = process_batch(predn, labelsn, iouv)
+
+                stats.append((correct, pred[:, 4], pred[:, 5], labels[:, 0]))  # (correct, conf, pcls, tcls)
+
+        if (i + 1) == len(loader):
+            stats = [torch.cat(x, 0).cpu().numpy() for x in zip(*stats)]  # to numpy
+            if len(stats) and stats[0].any():
+                tp, fp, p, r, f1, ap, ap_class = ap_per_class(*stats)
+                ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
+                mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
+            nt = np.bincount(stats[3].astype(int), minlength=20)  # number of targets per class
+
+            print(mp, mr, map50, map)
+
+            mp, mr, map50, map = detect_metric.ap_per_class()
+            print(mp, mr, map50, map)
 
     return metric
