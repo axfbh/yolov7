82fa8c8976b8e8dd5c9ca2a73152ab65379fafef
cgm <501704164@qq.com>
2024-04-25 19:14:50 +0800

    update

--- ops/model/backbone/elandarknet53.py
+++ ops/model/backbone/elandarknet53.py
@@ -85,6 +85,8 @@ class ElanDarkNet53(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(1024, num_classes)
 
+        self.reset_parameters()
+
     def reset_parameters(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
